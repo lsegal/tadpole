5760605f91f3a42d5167d6ebbca321e7176cad8a
Loren Segal <lsegal@soen.ca>
2008-06-12 10:23:40 -0400

    Update benchmark to use example1 (example2 changes too much)

--- benchmarks/run-caching.rb
+++ benchmarks/run-caching.rb
@@ -1,20 +1,20 @@
 require "benchmark"
 require File.dirname(__FILE__) + '/../lib/tadpole'
 
-Tadpole.register_template_path File.dirname(__FILE__) + '/../examples/example2'
+Tadpole.register_template_path File.dirname(__FILE__) + '/../examples/example1'
 
-TESTS = 100
+TESTS = 10_000
 
 Benchmark.bmbm do |b|
   b.report("no-cache") do 
     Tadpole.caching = false
-    t = Tadpole('tadpole/html/readme').new
-    TESTS.times { t.run }
+    t = Tadpole('custom/html').new
+    TESTS.times { t.run(:object => 1) }
   end
 
   b.report("cache   ") do 
     Tadpole.caching = true
-    t = Tadpole('tadpole/html/readme').new
-    TESTS.times { t.run }
+    t = Tadpole('custom/html').new
+    TESTS.times { t.run(:object => 1) }
   end
 end
